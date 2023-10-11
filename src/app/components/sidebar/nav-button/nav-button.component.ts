8744c7f6b24793992f2a4def10542f00d84890d2
Icaro Andrade <iba1987@gmail.com>
2023-10-10 23:37:17 -0300

    feat(sidebar): add nav-button component

---
+++ src/app/components/sidebar/nav-button/nav-button.component.ts
@@ -0,0 +1,17 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-nav-button',
+  templateUrl: './nav-button.component.html',
+  styleUrls: ['./nav-button.component.css'],
+})
+export class NavButtonComponent {
+  @Input() text = '';
+  @Input() icon = '';
+  @Input() selected = false;
+  @Output() click = new EventEmitter();
+
+  onClick() {
+    this.click.emit();
+  }
+}
