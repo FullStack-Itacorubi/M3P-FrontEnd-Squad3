be1723c153896299607d61eef570b0d9ac2e6a93
Kananda Degues <kananda.degues@gmail.com>
2023-10-12 16:01:31 -0300

    feat(user): add interface user

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
 
+export interface user{
+  fullname: string;
+  genre: string;
+  cpf: string;
+  phone: string;
+  type: string;
+  email: string;
+  password: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
