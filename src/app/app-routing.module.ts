c9ae5ddd59c0b5ec7500a7206aba6d543db5e344
Icaro <iba1987@gmail.com>
2023-10-11 21:11:22 -0300

    feat(stats): add page to layout

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,18 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { StatsComponent } from './pages/stats/stats.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children: [
+      {
+        path: '',
+        component: StatsComponent,
+      },
+    ],
   },
   {
     path: 'sidebar',
