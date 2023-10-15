d2ed48474187b7d0589fde659ebbc1f0583cafb3
Kananda Degues <kananda.degues@gmail.com>
2023-10-15 19:02:17 -0300

    feat(patient): update layout component and app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { PatientComponent } from './pages/patient/patient.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children:[{
+      path:"pacientes",
+      component: PatientComponent,
+    }]
   },
   {
     path: 'sidebar',
     component: SidebarComponent,
   },
+  {
+    path: ' pacientes',
+    component: PatientComponent,
+  }
 ];
 
 @NgModule({
