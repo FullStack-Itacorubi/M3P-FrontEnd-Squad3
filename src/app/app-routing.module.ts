45e133021f2c0dc75b34d47815480ea9f29b5c84
Kananda Degues <kananda.degues@gmail.com>
2023-10-15 16:08:27 -0300

    feat(exam): update layout component and app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ExamComponent } from './pages/exam/exam.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children:[{
+      path:"exames",
+      component: ExamComponent,
+    }]
   },
   {
     path: 'sidebar',
     component: SidebarComponent,
   },
+  {
+    path: 'exames',
+    component: ExamComponent,
+  }
 ];
 
 @NgModule({
