da5bd337983c104dd013602fe6713ad496e9707c
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-15 10:54:04 -0300

    feat: add ReactiveForm and HttpClient modules to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,29 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { NgIconsModule } from '@ng-icons/core';
+import * as IonIcons from '@ng-icons/ionicons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
-import { NgIconsModule } from '@ng-icons/core';
-import * as IonIcons from '@ng-icons/ionicons';
+import { LoginComponent } from './pages/login/login.component';
 
-@NgModule({
-  declarations: [AppComponent, LayoutComponent],
+@NgModule( {
+  declarations: [
+    AppComponent,
+    LayoutComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NgIconsModule.withIcons({ ...IonIcons }),
+    ReactiveFormsModule,
+    HttpClientModule,
+    NgIconsModule.withIcons( { ...IonIcons } ),
   ],
   providers: [],
-  bootstrap: [AppComponent],
-})
-export class AppModule {}
+  bootstrap: [ AppComponent ],
+} )
+export class AppModule { }
