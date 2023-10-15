4817c70dff9ca6b33720f4a55e93945e29e34b34
Kananda Degues <kananda.degues@gmail.com>
2023-10-15 17:28:54 -0300

    feat(exam): add interface exam

--- src/app/services/exam.service.ts
+++ src/app/services/exam.service.ts
@@ -1,5 +1,15 @@
 import { Injectable } from '@angular/core';
 
+export interface exam{
+  examName: string;
+  examDate: string;
+  examHour: string;
+  ExamType: string;
+  laboratory: string;
+  documentUrl: string;
+  results: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
