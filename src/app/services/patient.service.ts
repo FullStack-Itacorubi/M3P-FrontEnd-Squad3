e7dc306d589dea1ce67755ebcfeab378f79fa640
Kananda Degues <kananda.degues@gmail.com>
2023-10-16 20:12:18 -0300

    feat(patient): add interface patient in service

--- src/app/services/patient.service.ts
+++ src/app/services/patient.service.ts
@@ -1,5 +1,31 @@
 import { Injectable } from '@angular/core';
 
+export interface patient {
+  fullname: string;
+  genre: string;
+  birthdate: string;
+  cpf: string;
+  rg: string;
+  civilStatus: string;
+  placeOfBirth: string;
+  email: string;
+  phone: string;
+  emergencyContact: string;
+  allergyList: string;
+  specificCareList: string;
+  healthInsurance: string;
+  healthInsuranceNumber: string;
+  healthInsuranceValidity: string;
+  publicPlace: string;
+  number: string;
+  neighborhood: string;
+  city: string;
+  state: string;
+  cep: string;
+  complement: string;
+  referencePoint: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
