8a8765edfce77919aa0b0be7df383dc3adf33291
Marcos Hernandez <marcosh.jimenezh@gmail.com>
2023-10-17 02:09:44 -0300

    feat(exercise): update layout component & app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ExerciseComponent } from './exercise/exercise.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children:[{
+      path:"exercicios",
+      component: ExerciseComponent,
+    }]  
   },
   {
     path: 'sidebar',
     component: SidebarComponent,
   },
+  {
+    path: 'exercicios',
+    component: ExerciseComponent,
+  }
 ];
 
 @NgModule({
