c9694fcd6c5ca3b037c848c95cf29dc157f866ed
Kananda Degues <kananda.degues@gmail.com>
2023-10-17 08:57:51 -0300

    feat(diet): update layout component and app-routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,25 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LayoutComponent } from './components/layout/layout.component';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { DietComponent } from './pages/diet/diet.component';
 
 const routes: Routes = [
   {
     path: '',
     component: LayoutComponent,
+    children: [{
+      path: "dietas",
+      component: DietComponent,
+    }]
   },
   {
     path: 'sidebar',
     component: SidebarComponent,
   },
+  {
+    path: 'dietas',
+    component: DietComponent,
+  }
 ];
 
 @NgModule({
