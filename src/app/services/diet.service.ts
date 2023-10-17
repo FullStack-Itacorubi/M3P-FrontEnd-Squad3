d442a91784868ce88dfc68db456db5cbc6b44e5a
Kananda Degues <kananda.degues@gmail.com>
2023-10-17 20:10:11 -0300

    feat(diet): add interface diet

--- src/app/services/diet.service.ts
+++ src/app/services/diet.service.ts
@@ -1,5 +1,13 @@
 import { Injectable } from '@angular/core';
 
+export interface diet {
+  dietName: string;
+  type: string;
+  dietDate: string;
+  dietTime: string;
+  description: string;
+}
+
 @Injectable({
   providedIn: 'root'
 })
