027eebab44dd5c72a4b5347edf0450640e4589d9
Adir Silva Filho <tarukagahobenu@gmail.com>
2023-10-18 19:47:27 -0300

    feat: add modals at app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HTTP_INTERCEPTORS, HttpClientModule } from '@angular/common/http';
 import { NgIconsModule } from '@ng-icons/core';
 import * as IonIcons from '@ng-icons/ionicons';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LayoutComponent } from './components/layout/layout.component';
+import { GlobalHttpErrorHandlerInterceptor } from './middlewares/global-http-error-handler.interceptor';
 import { LoginComponent } from './pages/login/login.component';
+import { GenericModalComponent } from './shared/components/generic-modal/generic-modal.component';
+import { ResetPasswordComponent } from './components/reset-password/reset-password.component';
+import { ContactAdminComponent } from './components/contact-admin/contact-admin.component';
 
 @NgModule( {
   declarations: [
     AppComponent,
     LayoutComponent,
     LoginComponent,
+    GenericModalComponent,
+    ResetPasswordComponent,
+    ContactAdminComponent,
   ],
   imports: [
     BrowserModule,
@@ -23,7 +30,13 @@ import { LoginComponent } from './pages/login/login.component';
     HttpClientModule,
     NgIconsModule.withIcons( { ...IonIcons } ),
   ],
-  providers: [],
+  providers: [
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: GlobalHttpErrorHandlerInterceptor,
+      multi: true
+    }
+  ],
   bootstrap: [ AppComponent ],
 } )
 export class AppModule { }
