d776333ecf6783f46c4ca51a61860c592ca89188
Fabiola-dev <fabiola_magalhaes@estudante.sesisenai.org.br>
2023-10-21 14:42:10 -0300

    form + msg error

--- src/app/pages/query/query.component.ts
+++ src/app/pages/query/query.component.ts
@@ -1,4 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+
+interface Queriesinfos {
+  motive: FormControl<string | null>;
+  date: FormControl<string | null>;
+  time: FormControl<string | null>;
+  description: FormControl<string | null>;
+  medication: FormControl<string | null>;
+  dosage: FormControl<string | null>;
+  status: FormControl<string | null>;
+}
 
 @Component({
   selector: 'app-query',
@@ -6,5 +17,63 @@ import { Component } from '@angular/core';
   styleUrls: ['./query.component.css']
 })
 export class QueryComponent {
+
+  formQuery: FormGroup<Queriesinfos> = new FormGroup({
+    motive: new FormControl(''),
+    date: new FormControl(''),
+    time: new FormControl(''),
+    description: new FormControl(''),
+    medication: new FormControl(''),
+    dosage: new FormControl(''),
+    status: new FormControl(''),
+  });
+
+  ngOnInit(): void {
+    this.initQueryForm();
+  }
+
+  initQueryForm() {
+    this.formQuery = new FormGroup({
+      motive: new FormControl('', [
+        Validators.required,
+        Validators.minLength(8),
+        Validators.maxLength(64),
+      ]),
+      date: new FormControl('', [Validators.required]),
+      time: new FormControl('', [Validators.required]),
+      description: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(16), 
+        Validators.maxLength(1024)
+      ]),
+      medication: new FormControl(''),
+      dosage: new FormControl('', [
+        Validators.required, 
+        Validators.minLength(16), 
+        Validators.maxLength(256)
+      ]),
+      status: new FormControl('', [Validators.required]),
+    });
+  }
+
+  registerQueries() {
+    if (!this.formQuery.valid) {
+      alert('Formulário inválido, por favor insira ou corrija seus dados!');
+    } else {
+      alert('Dados cadastrados com sucesso!');
+    }
+
+    const query = {
+      motive: this.formQuery.value.motive!,
+      date: this.formQuery.value.date!,
+      time: this.formQuery.value.time!,
+      description: this.formQuery.value.description,
+      medication: this.formQuery.value.medication!,
+      dosage: this.formQuery.value.dosage!,
+      status: this.formQuery.value.status!,
+    };
+
+    this.initQueryForm();
+  }
 
 }
