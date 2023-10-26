006580748f9a8a2f808da8e08d88d2a5dce2308b
Icaro <iba1987@gmail.com>
2023-10-26 17:04:25 -0300

    fix(stats): import css to style scrollbar

--- src/app/pages/stats/stats.component.ts
+++ src/app/pages/stats/stats.component.ts
@@ -20,7 +20,7 @@ type StatsTabOption = {
 @Component({
   selector: 'app-stats',
   templateUrl: './stats.component.html',
-  styleUrls: ['./stats.component.css'],
+  styleUrls: ['./stats.component.css', '../../app.component.css'],
 })
 export class StatsComponent implements OnInit {
   menuOptions: StatsTabOption[] = [
