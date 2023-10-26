1d6a60559657a149f0eb55ec551bf9cf92fdf11f
Icaro <iba1987@gmail.com>
2023-10-25 21:10:05 -0300

    feat(stats): update dashboard to use users table

--- src/app/components/stats/tables/user-table-row/user-table-row.component.ts
+++ src/app/components/stats/tables/user-table-row/user-table-row.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { User } from 'src/app/shared/utils/types';
 
 @Component({
   selector: 'app-user-table-row',
   templateUrl: './user-table-row.component.html',
-  styleUrls: ['./user-table-row.component.css']
+  styleUrls: ['./user-table-row.component.css'],
 })
 export class UserTableRowComponent {
-
+  @Input() user?: User;
 }
