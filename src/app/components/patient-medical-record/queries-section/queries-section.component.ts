a61d5910f0d2a2905844a2b68312ed4faee1c389
Icaro <iba1987@gmail.com>
2023-10-26 23:41:13 -0300

    feat(patient med record): move queries data to query section component

--- src/app/components/patient-medical-record/queries-section/queries-section.component.ts
+++ src/app/components/patient-medical-record/queries-section/queries-section.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { QueryResponse } from 'src/app/shared/utils/types';
 
 @Component({
   selector: 'app-queries-section',
   templateUrl: './queries-section.component.html',
-  styleUrls: ['./queries-section.component.css']
+  styleUrls: ['./queries-section.component.css'],
 })
 export class QueriesSectionComponent {
-
+  @Input() queries: QueryResponse[] = [];
 }
