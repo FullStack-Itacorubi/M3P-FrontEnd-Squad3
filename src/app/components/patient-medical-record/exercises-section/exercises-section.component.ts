14fab906516636f08c6e8cd5b3390f48a9064184
Icaro <iba1987@gmail.com>
2023-10-26 23:45:14 -0300

    feat(patient med record): move exercises data to exercise section component

--- src/app/components/patient-medical-record/exercises-section/exercises-section.component.ts
+++ src/app/components/patient-medical-record/exercises-section/exercises-section.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Exercise } from 'src/app/shared/utils/types';
 
 @Component({
   selector: 'app-exercises-section',
   templateUrl: './exercises-section.component.html',
-  styleUrls: ['./exercises-section.component.css']
+  styleUrls: ['./exercises-section.component.css'],
 })
 export class ExercisesSectionComponent {
-
+  @Input() exercises: Exercise[] = [];
 }
