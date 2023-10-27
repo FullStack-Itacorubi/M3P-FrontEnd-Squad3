ccc92687d4827b4d24d9e8edb4d02fdcc83780fc
Kananda Degues <kananda.degues@gmail.com>
2023-10-26 22:51:04 -0300

    feat(logs): init logs in component.ts

--- src/app/pages/logs/logs.component.ts
+++ src/app/pages/logs/logs.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { LogsService } from 'src/app/shared/services/logs.service';
+import { Logs } from 'src/app/shared/utils/types';
 
 @Component({
   selector: 'app-logs',
   templateUrl: './logs.component.html',
-  styleUrls: ['./logs.component.css']
+  styleUrls: ['./logs.component.css', '../../app.component.css']
 })
-export class LogsComponent {
+export class LogsComponent implements OnInit {
+
+logs: Logs[] = [];
+
+constructor( private logsService: LogsService ){}
+
+async ngOnInit() {
+  this.logs = await this.logsService.getLogs();
+}
 
 }
