064974bcb126fa673861ce620298d72ca52831d5
Kananda Degues <kananda.degues@gmail.com>
2023-10-26 21:49:50 -0300

    feat(logs): create getLogs() on service.ts

--- src/app/shared/services/logs.service.ts
+++ src/app/shared/services/logs.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { environment } from '../utils/environment';
+import axios from 'axios';
+import { Logs } from '../utils/types';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LogsService {
-
+  private baseUrl = environment.API_BASE_URL;
+  
   constructor() { }
+
+  async getLogs(){
+    await axios.get<Logs[]>(`${this.baseUrl}/logs`);
+  }
+
 }
