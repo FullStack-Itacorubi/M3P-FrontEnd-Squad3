06952853db29df4337f9abc9960adb7c25c50322
Kananda Degues <kananda.degues@gmail.com>
2023-10-26 22:43:47 -0300

    feat(logs): create getDate() to get the timestamp

--- src/app/components/logs-table-row/logs-table-row.component.ts
+++ src/app/components/logs-table-row/logs-table-row.component.ts
@@ -4,9 +4,14 @@ import { Logs } from 'src/app/shared/utils/types';
 @Component({
   selector: 'app-logs-table-row',
   templateUrl: './logs-table-row.component.html',
-  styleUrls: ['./logs-table-row.component.css']
+  styleUrls: ['./logs-table-row.component.css'],
 })
 export class LogsTableRowComponent {
-  @Input() logs? : Logs;
+  @Input() logs?: Logs;
 
+  getDate() {
+    if (!this.logs) return;
+      const date = new Date(this.logs.timestamp);
+      return date.toLocaleString('pt-BR');
+  }
 }
