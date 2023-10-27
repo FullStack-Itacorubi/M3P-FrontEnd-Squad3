638a7a3700153ae57aaf3885792b0851e170cf63
Icaro <iba1987@gmail.com>
2023-10-26 23:47:29 -0300

    feat(patient med record): move diets data to diet section component

--- src/app/components/patient-medical-record/diets-section/diets-section.component.ts
+++ src/app/components/patient-medical-record/diets-section/diets-section.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Diet } from 'src/app/shared/utils/types';
 
 @Component({
   selector: 'app-diets-section',
   templateUrl: './diets-section.component.html',
-  styleUrls: ['./diets-section.component.css']
+  styleUrls: ['./diets-section.component.css'],
 })
 export class DietsSectionComponent {
-
+  @Input() diets: Diet[] = [];
 }
