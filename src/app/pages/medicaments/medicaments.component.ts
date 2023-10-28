c91d091ea960813649972263768be263637f3b97
Icaro <iba1987@gmail.com>
2023-10-28 13:23:34 -0300

    feat(medicament): populate form

--- src/app/pages/medicaments/medicaments.component.ts
+++ src/app/pages/medicaments/medicaments.component.ts
@@ -4,12 +4,43 @@ import { ActivatedRoute } from '@angular/router';
 import { MedicamentService } from 'src/app/shared/services/medicament.service';
 import { Medicament } from 'src/app/shared/utils/types';
 
+const MedicamentTypesValues = {
+  Cápsula: 'CAPSULE',
+  Comprimido: 'PILL',
+  Líquido: 'LIQUID',
+  Creme: 'CREAM',
+  Gel: 'GEL',
+  Inalação: 'INHALATION',
+  Injeção: 'INJECTION',
+  Spray: 'SPRAY',
+};
+
+type MedicamentType =
+  | 'Cápsula'
+  | 'Comprimido'
+  | 'Líquido'
+  | 'Creme'
+  | 'Gel'
+  | 'Inalação'
+  | 'Injeção'
+  | 'Spray';
+
+const MedicamentUnitsValues = {
+  mg: 'MG',
+  mcg: 'MCG',
+  g: 'G',
+  ml: 'ML',
+  '%': 'PERCENTAGE',
+};
+
+type MedicamentUnit = 'mg' | 'mcg' | 'g' | 'ml' | '%';
+
 type Medicamentsinfos = {
-  medicamentsName: FormControl<string | null>;
+  name: FormControl<string | null>;
   date: FormControl<string | null>;
   time: FormControl<string | null>;
   type: FormControl<string | null>;
-  amount: FormControl<number | null>;
+  quantity: FormControl<number | null>;
   unit: FormControl<string | null>;
   observations: FormControl<string | null>;
   status: FormControl<boolean | null>;
@@ -48,7 +79,7 @@ export class MedicamentsComponent implements OnInit {
   initMedicamentsForm() {
     const today = new Date();
     return new FormGroup<Medicamentsinfos>({
-      medicamentsName: new FormControl('', [
+      name: new FormControl('', [
         Validators.required,
         Validators.minLength(5),
         Validators.maxLength(100),
@@ -60,7 +91,7 @@ export class MedicamentsComponent implements OnInit {
         Validators.required,
       ]),
       type: new FormControl('', [Validators.required]),
-      amount: new FormControl(null, [Validators.required]),
+      quantity: new FormControl(null, [Validators.required]),
       unit: new FormControl('', [Validators.required]),
       observations: new FormControl('', [
         Validators.required,
@@ -73,7 +104,19 @@ export class MedicamentsComponent implements OnInit {
     });
   }
 
-  populateForm(medicament: Medicament) {}
+  populateForm(medicament: Medicament) {
+    const type = medicament.type as MedicamentType;
+    const unit = medicament.unit as MedicamentUnit;
+    this.formMedicaments.get('name')?.setValue(medicament.name);
+    this.formMedicaments.get('date')?.setValue(medicament.date);
+    this.formMedicaments.get('time')?.setValue(medicament.time);
+    this.formMedicaments.get('type')?.setValue(MedicamentTypesValues[type]);
+    this.formMedicaments.get('quantity')?.setValue(medicament.quantity);
+    this.formMedicaments.get('unit')?.setValue(MedicamentUnitsValues[unit]);
+    this.formMedicaments.get('observations')?.setValue(medicament.observations);
+    this.formMedicaments.get('status')?.setValue(medicament.status);
+    this.formMedicaments.get('status')?.enable();
+  }
 
   registerMedicament() {
     if (!this.formMedicaments.valid) {
@@ -89,11 +132,11 @@ export class MedicamentsComponent implements OnInit {
       .join('/');
 
     const medicament: Medicament = {
-      name: this.formMedicaments.value.medicamentsName!,
+      name: this.formMedicaments.value.name!,
       date: dateFormated!,
       time: this.formMedicaments.value.time! + ':00',
       type: this.formMedicaments.value.type!,
-      quantity: this.formMedicaments.value.amount!,
+      quantity: this.formMedicaments.value.quantity!,
       unit: this.formMedicaments.value.unit!,
       observations: this.formMedicaments.value.observations!,
       status: this.formMedicaments.value.status!,
